3ed1338026669be2a57dc1a142e6e3bcd47a7fe7
Aditya <adidada023@gmail.com>
2023-01-07 02:57:00 +0530

    added project section

--- src/components/experience/Experience.js
+++ src/components/experience/Experience.js
@@ -1,11 +1,89 @@
 import React from 'react'
 import './Experience.css'
 
+import {BsPatchCheckFill} from 'react-icons/bs'
+
 function experience() {
   return (
     <section id='experience'>
-      <h5>What Skills I Have</h5>
+      <h5>The Skills I Have</h5>
       <h2>My Experience</h2>
+
+      <div className="container experience__container">
+        <div className="experience__frontend">
+            <h3>Frontend Development</h3>
+            <div className="experience__content">
+              <article className="experience__details">
+                 <BsPatchCheckFill className="experience__details-icon"/>
+                 <div>
+                 <h4>HTML</h4>
+                 <small className="text-light">Experienced</small>
+                 </div>
+              </article>
+              <article className="experience__details">
+                 <BsPatchCheckFill className="experience__details-icon"/>
+                 <div>
+                 <h4>HTML</h4>
+                 <small className="text-light">Experienced</small>
+                 </div>
+              </article>
+              <article className="experience__details">
+                 <BsPatchCheckFill className="experience__details-icon"/>
+                 <div>
+                 <h4>HTML</h4>
+                 <small className="text-light">Experienced</small>
+                 </div>
+              </article>
+              <article className="experience__details">
+                 <BsPatchCheckFill className="experience__details-icon"/>
+                 <div>
+                 <h4>HTML</h4>
+                 <small className="text-light">Experienced</small>
+                 </div>
+              </article>
+              <article className="experience__details">
+                 <BsPatchCheckFill className="experience__details-icon"/>
+                 <div>
+                 <h4>HTML</h4>
+                 <small className="text-light">Experienced</small>
+                 </div>
+              </article>
+            </div>
+        </div>
+        <div className="experience__backend">
+        <h3>Backend Development</h3>
+            <div className="experience__content">
+              <article className="experience__details">
+                 <BsPatchCheckFill className="experience__details-icon"/>
+                 <div>
+                 <h4>HTML</h4>
+                 <small className="text-light">Experienced</small>
+                 </div>
+              </article>
+              <article className="experience__details">
+                 <BsPatchCheckFill className="experience__details-icon"/>
+                 <div>
+                 <h4>HTML</h4>
+                 <small className="text-light">Experienced</small>
+                 </div>
+              </article>
+              <article className="experience__details">
+                 <BsPatchCheckFill className="experience__details-icon"/>
+                 <div>
+                 <h4>HTML</h4>
+                 <small className="text-light">Experienced</small>
+                 </div>
+              </article>
+              <article className="experience__details">
+                 <BsPatchCheckFill className="experience__details-icon"/>
+                 <div>
+                 <h4>HTML</h4>
+                 <small className="text-light">Experienced</small>
+                 </div>
+              </article>
+            </div>
+        </div>
+      </div>
     </section>
   )
 }
--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -1,8 +1,79 @@
 import React from 'react'
+import './Portfolio.css'
+import IMG1 from '../../assets/portfolio1.jpg'
+import IMG2 from '../../assets/portfolio2.jpg'
+import IMG3 from '../../assets/portfolio3.jpg'
+import IMG4 from '../../assets/portfolio4.jpg'
+import IMG5 from '../../assets/portfolio5.png'
+import IMG6 from '../../assets/portfolio6.jpg'
 
 function Portfolio() {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h2>Portfolio</h2>
+      <div className="container portfolio__container">
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className="portfolio__item-cta">
+          <a href="" className='btn'>Github</a>
+          <a href="" className='btn btn-primary' target='_blank'>Live Demo</a>
+          </div>
+        </article>
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className="portfolio__item-cta">
+          <a href="" className='btn'>Github</a>
+          <a href="" className='btn btn-primary' target='_blank'>Live Demo</a>
+          </div>
+        </article>
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className="portfolio__item-cta">
+          <a href="" className='btn'>Github</a>
+          <a href="" className='btn btn-primary' target='_blank'>Live Demo</a>
+          </div>
+        </article>
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className="portfolio__item-cta">
+          <a href="" className='btn'>Github</a>
+          <a href="" className='btn btn-primary' target='_blank'>Live Demo</a>
+          </div>
+        </article>
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className="portfolio__item-cta">
+          <a href="" className='btn'>Github</a>
+          <a href="" className='btn btn-primary' target='_blank'>Live Demo</a>
+          </div>
+        </article>
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={IMG1} alt="" />
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <div className="portfolio__item-cta">
+          <a href="" className='btn'>Github</a>
+          <a href="" className='btn btn-primary' target='_blank'>Live Demo</a>
+          </div>
+        </article>
+      </div>
+    </section>
   )
 }
 
